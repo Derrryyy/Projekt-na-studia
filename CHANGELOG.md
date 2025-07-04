939d14afb3fbadd7b2f21734992e4e89049636ae
Oskar <konzyn17@gmail.com>
2025-07-04 11:49:42 +0200

    Gradle + HTTP pod Railway

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+[v1.0.1]
+Usunięto zbędne "=======HEAD" z index.html
+
+[v1.0.2]
+Dodano sekcje FAQ do navi
+
+[v1.0.3]
+Zamieniono index.html na home.html i dodano listę
--- src/main/Kalkulator.java
+++
@@ -1,9 +0,0 @@
-public class Kalkulator{
-public int add(int a, int b){
-return a + b;
-}
-public int divide(int a, int b){
-if (b==0) throw new IllegalArgumentException("Dzielenie przez zero");
-return a/b;
-}
-}
