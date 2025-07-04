fff011e289903b47b478b14c8f2ed20b930d3454
Oskar <konzyn17@gmail.com>
2025-07-04 17:36:03 +0200

    Dodano GitHub Pages workflow

---
+++ calculator-js/script.js
@@ -0,0 +1,5 @@
+function calculate() {
+  const a = parseFloat(document.getElementById('a').value);
+  const b = parseFloat(document.getElementById('b').value);
+  document.getElementById('result').textContent = a + b;
+}
